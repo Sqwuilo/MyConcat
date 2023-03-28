ce320ad23534cdc37ec1489eb03df5b25592881d
sqwuilo <serezhaonufrienko@yandex.ru>
2023-03-28 20:56:11 +0300

    Initial commit

---
+++ src/Collection.java
@@ -0,0 +1,10 @@
+public class Collection {
+
+
+    public String myConcat(String line1, String line2){
+        String result = line1 + line2;
+        return result;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Collection collection = new Collection();
+
+        System.out.println(collection.myConcat("abcdefg", "ABCDERF"));
+    }
+}
